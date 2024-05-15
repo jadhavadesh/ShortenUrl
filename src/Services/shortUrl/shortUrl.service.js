5480438969a886d8fdbbad7f7fb12703ddb7adb2
jadhavadesh <adeshjadhav2700@gmail.com>
2024-05-15 17:40:10 +0530

    updated server url

--- src/Services/shortUrl/shortUrl.service.js
+++ src/Services/shortUrl/shortUrl.service.js
@@ -10,17 +10,17 @@ export const shortUrlService = {
 
 // shorte url Service
 function shortenUrlService(url) {
-  return http.post(`${process.env.SERVER_URL}/api/shorten`, url);
+  return http.post(`/api/shorten`, url);
 }
 
 // get all urls
 function getAllUrlsService(url) {
-    return http.get(`${process.env.SERVER_URL}/api/urls`);
+    return http.get(`/api/urls`);
 }
 
 // Update short code
 function updateShortCodeService(payload) {
-    return http.post(`${process.env.SERVER_URL}/api/update-short-url`, payload);
+    return http.post(`/api/update-short-url`, payload);
 }
 
 
