e983c6af0cec676ca63606cd8cf99b90d4a461cb
jadhavadesh <adeshjadhav2700@gmail.com>
2024-05-15 16:48:30 +0530

    updated prod url

--- src/Services/shortUrl/shortUrl.service.js
+++ src/Services/shortUrl/shortUrl.service.js
@@ -10,17 +10,17 @@ export const shortUrlService = {
 
 // shorte url Service
 function shortenUrlService(url) {
-  return http.post(`${process.env.REACT_APP_BASE_URL}/api/shorten`, url);
+  return http.post(`${process.env.PROD_URL}/api/shorten`, url);
 }
 
 // get all urls
 function getAllUrlsService(url) {
-    return axios.get(`${process.env.REACT_APP_BASE_URL}/api/urls`);
+    return axios.get(`${process.env.PROD_URL}/api/urls`);
 }
 
 // Update short code
 function updateShortCodeService(payload) {
-    return axios.post(`${process.env.REACT_APP_BASE_URL}/api/update-short-url`, payload);
+    return axios.post(`${process.env.PROD_URL}/api/update-short-url`, payload);
 }
 
 
